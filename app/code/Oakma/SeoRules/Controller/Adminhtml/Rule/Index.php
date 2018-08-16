1987672d39c61e06f8a66023f4fee092ca37a426
Evgeniy Dubskiy <www.developer.local@gmail.com>
2018-08-16 17:44:07 +0300

    SeoRule

---
+++ app/code/Oakma/SeoRules/Controller/Adminhtml/Rule/Index.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Oakma\SeoRule\Controller\Adminhtml\Rule;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Oakma_SeoRule::rule';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Oakma_SeoRule::rule');
+        $resultPage->addBreadcrumb(__('Seo Rules'), __('Seo Rules'));
+        $resultPage->addBreadcrumb(__('Manage Rules'), __('Manage Rules'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Rules'));
+
+        return $resultPage;
+    }
+}
