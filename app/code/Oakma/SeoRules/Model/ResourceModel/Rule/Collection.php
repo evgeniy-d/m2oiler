917c0b7649c08185276d91d647e4a795ce8d639d
Evgeniy Dubskiy <www.developer.local@gmail.com>
2018-08-22 14:13:54 +0300

    SeoRules - store_ids

--- app/code/Oakma/SeoRules/Model/ResourceModel/Rule/Collection.php
+++ app/code/Oakma/SeoRules/Model/ResourceModel/Rule/Collection.php
@@ -2,6 +2,7 @@
 namespace Oakma\SeoRules\Model\ResourceModel\Rule;
 
 use \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use \Oakma\SeoRules\Api\Data\RuleInterface;
 
 /**
  * Class Collection
@@ -40,4 +41,60 @@ class Collection extends AbstractCollection
 		$this->_init('Oakma\SeoRules\Model\Rule', 'Oakma\SeoRules\Model\ResourceModel\Rule');
 	}
 
+	/**
+	 * Perform operations after collection load
+	 *
+	 * @param string $tableName
+	 * @param string|null $linkField
+	 * @return void
+	 */
+	protected function performAfterLoad($tableName, $linkField)
+	{
+		$linkedIds = $this->getColumnValues($linkField);
+		if (count($linkedIds)) {
+			$connection = $this->getConnection();
+			$select = $connection->select()->from(['cms_entity_store' => $this->getTable($tableName)])
+				->where('cms_entity_store.' . $linkField . ' IN (?)', $linkedIds);
+			$result = $connection->fetchAll($select);
+			if ($result) {
+				$storesData = [];
+				foreach ($result as $storeData) {
+					$storesData[$storeData[$linkField]][] = $storeData['store_id'];
+				}
+
+				foreach ($this as $item) {
+					$linkedId = $item->getData($linkField);
+					if (!isset($storesData[$linkedId])) {
+						continue;
+					}
+					$storeIdKey = array_search(Store::DEFAULT_STORE_ID, $storesData[$linkedId], true);
+					if ($storeIdKey !== false) {
+						$stores = $this->storeManager->getStores(false, true);
+						$storeId = current($stores)->getId();
+						$storeCode = key($stores);
+					} else {
+						$storeId = current($storesData[$linkedId]);
+						$storeCode = $this->storeManager->getStore($storeId)->getCode();
+					}
+					$item->setData('_first_store_id', $storeId);
+					$item->setData('store_code', $storeCode);
+					$item->setData('store_id', $storesData[$linkedId]);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Perform operations after collection load
+	 *
+	 * @return $this
+	 */
+	protected function _afterLoad()
+	{
+		foreach ($this->_items as $item) {
+			$item->setData(RuleInterface::STORE_IDS, explode(',', $item->getData(RuleInterface::STORE_IDS)));
+		}
+
+		return parent::_afterLoad();
+	}
 }
---
+++ app/code/Oakma/SeoRules/Model/ResourceModel/Rule/Relation/Store/ReadHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Oakma\SeoRules\Model\ResourceModel\Rule\Relation\Store;
+
+use Oakma\SeoRules\Api\Data\RuleInterface;
+use Magento\Framework\EntityManager\MetadataPool;
+use Magento\Framework\EntityManager\Operation\ExtensionInterface;
+
+/**
+ * Class ReadHandler
+ */
+class ReadHandler implements ExtensionInterface
+{
+    /**
+     * @var MetadataPool
+     */
+    protected $metadataPool;
+
+    /**
+     * @param MetadataPool $metadataPool
+     * @param Page $resourcePage
+     */
+    public function __construct(
+        MetadataPool $metadataPool
+    ) {
+        $this->metadataPool = $metadataPool;
+    }
+
+    /**
+     * @param RuleInterface $entity
+     * @param array $arguments
+     *
+     * @return object
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+	public function execute($entity, $arguments = [])
+	{
+        if ($entity->getId() && !is_array($entity->getData(RuleInterface::STORE_IDS))) {
+	        $entity->setData(RuleInterface::STORE_IDS, explode(',', $entity->getData(RuleInterface::STORE_IDS)));
+        }
+
+        return $entity;
+    }
+}
